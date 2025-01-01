c6ffdb3c6ead579b36aa863a139485691255bbf1
Likhith <likhithrj@gmail.com>
2025-01-01 20:59:38 +0530

    Loops

---
+++ practise.py
@@ -0,0 +1,27 @@
+#print numbers from 1 to 100
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+#reverse order 100 to 1
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+#Multiplication of number n 
+# n = int(input("Enter a number: "))
+# i = 1
+# while i <= 10:
+#     print(n * i)
+#     i += 1
+
+#Print a list
+nums = [1,4,5,166,173,23,25,67,87]
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
---
+++ while_loops.py
@@ -0,0 +1,9 @@
+# count = 1       #iterators
+# while count <= 5:       #iteration
+#     print("Hello",count)
+#     count += 1
+
+i = 5
+while i >= 0:
+    print("Hi",i)
+    i -= 1
